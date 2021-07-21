dded6065fa7a0e010619e347b1c3756a62fb20f0
David Park(Horyang) <ghfid86@gmail.com>
2021-07-21 12:46:27 +0900

    working on js - event listeners and etc

--- js/memo.js
+++ js/memo.js
@@ -1,11 +1,16 @@
-import memoService from './memoAction.js';
+//import memoService from './memoAction.js';
 
 export default class Memo {
-    constructor(id, content, position) {
+    constructor(id, content, position, size) {
 
         this.id = id; 
         this.content = content;
         this.position = position;
+        this.size = size;
+
+        this.draggerPivot = null;
+        this.dragged = null;
+        this.wrapper = null;
 
         this.createMemoDOM();
 
@@ -14,20 +19,69 @@ export default class Memo {
 
     createMemoDOM() {
         $(".wrap").append(`
-            <div id="memo_1"
-                    class="memo"
-                    style="top:100px; left:100px;">
-            <div class="header">
-                <h1 class="blind">Memo</h1>
-                <button class="btn_close"><span class="blind">Close</span></button>
-            </div>
-            <div class="content">
-            <div class="textarea"
-                    contenteditable="true"
-                    style="width:200px; height:100px;">Contents</div>
-            </div>
+            <div id="${this.id}"
+                class="memo"
+                style="top:${this.position.top}px; left:${this.position.left}px;">
+                <div class="header">
+                    <h1 class="blind">Memo</h1>
+                    <button class="btn_close"><span class="blind">Close</span></button>
+                </div>
+                <div class="content">
+                <div class="textarea"
+                        contenteditable="true"
+                        style="width:${this.size.width}px; height:${this.size.height}px;">${this.content}</div>
+                </div>
                 <button class="btn_size"><span class="blind">Adjust memo size</span></button>
             </div>
         `);
     }
+
+    bindDragEvent() {
+        this.draggerPivot = document.querySelector(`#${this.id} .header`);
+        this.draggerPivot.addEventListener('mousedown', this.startDragEvent(), false);
+
+        // enable memo being dragged, dropped on the wrap 
+        this.wrapper = document.querySelector('.wrap');
+        this.wrapper.addEventListener('dragEnter', this.dragEnter(), false);
+        this.wrapper.addEventListener('dragOver', this.dragEnter(), false);
+        this.wrapper.addEventListener('dropped', this.dragEnter(), false);
+        
+    }
+
+    startDragEvent = (e) => {
+        if(e.target.className !== 'header') { return false; }
+        this.dragged = document.querySelector(`#${this.id}`);
+        this.dragged.setAttribute('draggable', true);
+        this.dragged.addEventListener('startDragging');
+        this.dragged.addEventListener('stopDragging');
+
+        
+    } 
+
+    startDragging = (e) => {
+        
+    }
+
+    stopDragging = (e) => {
+
+    }
+
+
+
+
+    dragEnter = (e) => {
+        e.preventDefault();
+        return true;
+    }
+
+    dragOver = (e) => {
+        e.preventDefault();
+        
+    }
+
+    dropped = (e) => {
+        e.preventDefault();
+        return false;
+    }
+
 }
--- js/memoAction.js
+++ js/memoAction.js
@@ -1,3 +1,16 @@
 import Memo from './memo.js';
-import MemoService from './memoService';
+//import MemoService from './memoService';
 
+document.addEventListener("DOMContentLoaded", () => {
+    createMemo();
+});
+
+
+const createMemo = () => {
+    return new Memo(
+        'memo_1',
+        'Content here',
+        {top: 100, left: 100},
+        {width: 200, height: 100}    
+    );
+}
