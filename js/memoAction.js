aadf181b9f425e1cee08883e75c08b81c33b698b
David Park(Horyang) <ghfid86@gmail.com>
2021-07-23 13:22:00 +0900

    Now the memo correctly appears on RMB. Problem was misplaced pageX and pageY

--- js/memoAction.js
+++ js/memoAction.js
@@ -10,31 +10,19 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 
-// const createMemo = () => {
-//     // return new Memo(
-//     //     'memo_1',
-//     //     'Content here',
-//     //     {top: 100, left: 100},
-//     //     {width: 200, height: 100},
-//     //     1,
-//     //     1   
-//     // );
-    
-// }
-
 const createMemo = () => {
-    var background = document.getElementsByClassName('wrap')[0];
+    var background = document.getElementsByClassName('wrap')[0];  // why adding [0] is only possible..? also querySelector didnt work either
     background.addEventListener('contextmenu', (e) => {
         if(e.target !== e.currentTarget) return;
         e.preventDefault();
-        console.log(e.pageX, " " ,e.pageY);
+        console.log(e.pageX,e.pageY);
         console.log("count before returning memo: ", memoCount);
         memoCount += 1;
-        return memo = new Memo (
+        return memo = new Memo (   // why does this generate error on console : "memo is not defined - uncaught reference error"
             `memo_${memoCount}`,
             'Comment Here',
-            {top: e.pageX, left: e.pageY},
-            {width: 200, height: 100},
+            {top: e.pageY, left: e.pageX},   // currently having issue while generating memos. It appears on incorrect coordinates except the top left
+            {width: 200, height: 100},              // So stupid. I put top: e.pageX while it should top: e.pageY !!! DUMBSHIT. 
             memoCount
         );
     }, false);
